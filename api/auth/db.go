5ef8c2ac63531b11a9e5bed7fa22b565d3068d73
Adriano <adriano.selaviles@gmail.com>
2020-06-12 13:06:17 -0700

    authentication and storage (mock + mongodb)

---
+++ api/auth/db.go
@@ -0,0 +1,11 @@
+package auth
+
+// DB represents the storage needs of
+// the authenticator module
+type DB interface {
+	// TODO:
+	// PutUser()
+	// GetUser()
+	// UpdateUser()
+	// DeleteUser()
+}
---
+++ api/objects/tree.go
@@ -0,0 +1,35 @@
+package objects
+
+import (
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	errTreeHasNoID       = errors.New("tree has no id")
+	errTreeHasNoTaggedBy = errors.New("tree has no tagger")
+	errTreeHasNoSpecies  = errors.New("tree has no species")
+)
+
+// Tree represents the attributes of a tree
+type Tree struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	TaggedBy string             `json:"tagged_by" bson:"tagged_by"`
+	Species  string             `json:"species" bson:"species"`
+}
+
+// Validate validates a tree has all
+// mandatory fields populated
+func (t *Tree) Validate(checkID, checkTaggedBy bool) error {
+	if checkID && t.ID.String() == "" {
+		return errTreeHasNoID
+	}
+	if checkTaggedBy && t.TaggedBy == "" {
+		return errTreeHasNoTaggedBy
+	}
+	if t.Species == "" {
+		return errTreeHasNoSpecies
+	}
+	return nil
+}
