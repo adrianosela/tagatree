0cd4b53fc2ca553ef79544ed01f7ca78c77637e4
Adriano <adriano.selaviles@gmail.com>
2020-06-25 15:01:46 -0700

    eslint for react app, and add api

---
+++ app/src/api/Tree.js
@@ -0,0 +1,74 @@
+import config from './Config';
+
+export const getTree = (id) => {
+  return new Promise((resolve, reject) => {
+    localStorage.getItem('token')
+      .then(token => {
+        fetch(`${config.API_URL}/tree/${id}`, {
+          method: 'GET',
+          headers: { 'Authorization': token },
+        })
+          .then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+              resolve(response.json());
+            }
+            if (response.status === 401) {
+              reject({ error: 'permission denied' });
+            }
+            if (response.status === 404) {
+              reject({ error: 'tree not found' });
+            }
+            reject({ error: 'unknown error' });
+          })
+          .catch(error => reject(error));
+      });
+  });
+};
+
+export const postTree = (species, location) => {
+  return new Promise((resolve, reject) => {
+    localStorage.getItem('token')
+      .then(token => {
+        fetch(`${config.API_URL}/tree`, {
+          method: 'POST',
+          headers: { 'Authorization': token },
+          body: JSON.stringify({
+            species: species,
+            location: location
+          }),
+        })
+          .then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+              resolve(response.json());
+            }
+            console.log('got status {response.status}');
+            reject({ error: 'request failed' });
+          })
+          .catch(error => reject(error));
+      });
+  });
+};
+
+export const listTrees = (opts) => {
+  return new Promise((resolve, reject) => {
+    localStorage.getItem('token')
+      .then(userToken => {
+        fetch(`${config.API_URL}/trees`, {
+          method: (opts) ? 'POST' : 'GET',
+          body: opts,
+          headers: { 'Authorization': userToken },
+        })
+          .then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+              resolve(response.json());
+            }
+            console.log('got status {response.status}');
+            reject({ error: 'request failed' });
+          })
+          .catch(error => reject(error));
+      });
+  });
+};
